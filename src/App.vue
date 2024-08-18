29e243a42cd07ee93f4c88597a9d4b30081c4695
Shakhzod <93878055+Shakhzod0307@users.noreply.github.com>
2024-08-18 06:49:20 +0500

    updating project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button style="padding: 10px 30px; color:red; font-size: 25px" @click="count++">{{ count }}</button>
+  <h1>{{message}}</h1>
+  <button @click="clickMe">Click Me</button>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      count: 1,
+      message: "Hello",
+    };
+  },
+  methods:{
+    clickMe(){
+      console.log('Clicked');
+      this.message = "Updated";
+    }
+  }
+};
+
+</script>
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
